2c7d18e46219abeb2f293918e882ece2b7732389
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-22 01:14:25 -0800

    Updated PavedPath linting

--- paved_path/__init__.py
+++ paved_path/__init__.py
@@ -1,3 +1,4 @@
+"""A modified of pathlib.Path that adds extra functionality."""
 from __future__ import annotations
 
 import shutil
@@ -7,38 +8,36 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     import os
-    from typing import Any, Optional, Self, TypeVar
+    from typing import Self, TypeVar
 
-    PathableType = TypeVar(
-        "PathableType", str, bytes, os.PathLike[str], int, datetime, date, float
-    )
+    PathableType = TypeVar("PathableType", str, bytes, os.PathLike[str], int, datetime, date, float)
 
 
-# Python's Path implementation is a little bit unsuaul
+# Python's Path implementation  is a little bit unsuaul
 # Using type(Path()) is required to subclass Path but it may break in the future
-class PavedPath((type(Path()))):
+class PavedPath(type(Path())):
     """A modified of pathlib.Path that adds extra functionality."""
 
-    def __new__(cls, *args: PathableType, **kwargs: Any) -> Self:
+    def __new__(cls, *args: PathableType) -> Self:
+        """Convert all arguments to Path objects and passes them to the Path constructor."""
         # TypeErrors occur in __new__ so values need to be cast here before __init__
         path_fragments = [cls._convert_to_path(partial_path) for partial_path in args]
         return super().__new__(cls, *path_fragments)
 
     # Add extra instance variables used for caching
     def __init__(self, *_args: PathableType) -> None:
+        """Initialize the path object and set the cached values to None."""
         self.read_bytes_cached_value = None
         self.cached_content_value = None
 
     # When values are appended to a path the new path should be validated
     def __truediv__(self, key: PathableType) -> Self:
         """Append a value to the path and return a new path object."""
-        full_path = super().__truediv__(self._convert_to_path(key))
-        return full_path
+        return super().__truediv__(self._convert_to_path(key))
 
     @classmethod
     def _convert_to_path(cls, value: PathableType) -> os.PathLike[str]:
-        """Converts a string, bytes, os.PathLike, int, datetime, date or float to a Path object."""
-
+        """Convert a string, bytes, os.PathLike, int, datetime, date or float to a Path object."""
         if isinstance(value, (datetime, date, float, int, str, bytes)):
             # datetime - 123.456
             # date - 2000-01-01
@@ -54,9 +53,8 @@ class PavedPath((type(Path()))):
         """Get the mtime of a file as a timezone aware datetime object."""
         return datetime.fromtimestamp(self.stat().st_mtime).astimezone()
 
-    def up_to_date(self, timestamp: Optional[datetime] = None) -> bool:
-        """Checks if the file is up to date with respect to a given timestamp."""
-
+    def up_to_date(self, timestamp: datetime | None = None) -> bool:
+        """Check if the file is up to date with respect to a given timestamp."""
         # If file does not exist it can't be up to date
         if not self.exists():
             return False
@@ -68,48 +66,49 @@ class PavedPath((type(Path()))):
         # When there is a file and a timestamp make the timestamp timezone aware and compare it to the file's mtime
         return self.aware_mtime() > timestamp.astimezone()
 
-    def outdated(self, timestamp: Optional[datetime] = None) -> bool:
-        """Checks if the file is outdated with respect to a given timestamp."""
-
+    def outdated(self, timestamp: datetime | None = None) -> bool:
+        """Check if the file is outdated with respect to a given timestamp."""
         return not self.up_to_date(timestamp)
 
-    def write(self, content: bytes | str):
+    def write(self, content: bytes | str) -> None:
         """Write a bytes or a str object to a file, and will automatically create the directory if needed."""
-
         self.parent.mkdir(parents=True, exist_ok=True)
 
         if isinstance(content, bytes):
-            with self.open("wb") as file:
-                file.write(
-                    content
-                )  # Silly to have this listed twice, but it's required for Pylance
+            self.write_bytes(content)
         else:
-            with self.open("w", encoding="utf-8") as file:
-                file.write(
-                    content
-                )  # Silly to have this listed twice, but it's required for Pylance
+            self.write_text(content, encoding="utf-8")
 
-    def delete(self):
+    def delete(self) -> None:
         """Delete a folder or a file without having to worry about which it is."""
-
         if self.exists():
             if self.is_file():
                 self.unlink()
             else:
                 shutil.rmtree(self)
 
-    def read_text_cached(self, reload: bool = False, encoding: str = "utf-8"):
-        """Read a file and cache the result to avoid reading the file multiple times."""
-
-        if not self.cached_content_value or reload:
+    def read_text_cached(self, encoding: str = "utf-8") -> str:
+        """Read a file in text mode and cache the result to avoid reading the file multiple times."""
+        if not self.cached_content_value:
             self.cached_content_value = self.read_text(encoding=encoding)
 
         return self.cached_content_value
 
-    def read_bytes_cached(self, reload: bool = False):
-        """Read a file and cache the result to avoid reading the file multiple times."""
-
-        if not self.read_bytes_cached_value or reload:
+    def read_bytes_cached(self) -> bytes:
+        """Read a file in bytes mode and cache the result to avoid reading the file multiple times."""
+        if not self.read_bytes_cached_value:
             self.read_bytes_cached_value = self.read_bytes()
+
+        return self.read_bytes_cached_value
+
+    def force_read_text_cached(self, encoding: str = "utf-8") -> str:
+        """Open a file in text mode and cache the value even if a value already exists."""
+        self.cached_content_value = self.read_text(encoding=encoding)
+
+        return self.cached_content_value
+
+    def force_read_bytes_cached(self) -> bytes:
+        """Open a file in byte mode and cache the value even if a value already exists."""
+        self.read_bytes_cached_value = self.read_bytes()
 
         return self.read_bytes_cached_value
--- tests/test_extended_path.py
+++ tests/test_extended_path.py
@@ -1,3 +1,4 @@
+"""Test the PavedPath class."""
 from __future__ import annotations
 
 from datetime import date, datetime
@@ -13,10 +14,12 @@ if TYPE_CHECKING:
 
 
 class TestConversions:
+    """Test that various types of input are correctly converted to a PavedPath."""
+
     @pytest.mark.parametrize(
-        "input_value,expected_output",
+        ("input_value", "expected_output"),
         [
-            (datetime(2020, 1, 1).timestamp(), "1577865600.0"),
+            (datetime(2020, 1, 1).astimezone().timestamp(), "1577865600.0"),
             (date(2020, 1, 1), "2020-01-01"),
             (123.456, "123.456"),
             (123, "123"),
@@ -24,46 +27,56 @@ class TestConversions:
             (PavedPath("abc"), "abc"),
         ],
     )
-    def test_conversion(self, input_value: PathableType, expected_output: str):
+    def test_conversion(self, input_value: PathableType, expected_output: str) -> None:
         """Test that various types of input are correctly converted to a PavedPath."""
         assert PavedPath(input_value) == PavedPath(expected_output)
 
 
 class TestUpToDate:
-    def test_up_to_date_no_file(self):
-        timestamp = datetime.now()
+    """Test the up_to_date method."""
+
+    def test_up_to_date_no_file(self) -> None:
+        """Test that up_to_date returns False if the file does not exist."""
         file = PavedPath("Temp Test Files/test_up_to_date_no_file.ext")
+        timestamp = datetime.now().astimezone()
         file.parent.delete()
         assert not file.up_to_date(timestamp)
 
-    def test_up_to_date_no_file_or_timestamp(self):
+    def test_up_to_date_no_file_or_timestamp(self) -> None:
+        """Test that up_to_date returns False if the file does not exist and no timestamp is given."""
         file = PavedPath("Temp Test Files/test_up_to_date_no_file.ext")
         file.parent.delete()
         assert not file.up_to_date()
 
-    def test_up_to_date_new_file(self):
-        timestamp = datetime.now()
+    def test_up_to_date_new_file(self) -> None:
+        """Test that up_to_date returns True if the file is newer than the timestamp."""
         file = PavedPath("Temp Test Files/test_up_to_date_new_file.ext")
+        timestamp = datetime.now().astimezone()
         file.write("Text")
         assert file.up_to_date(timestamp)
         file.parent.delete()
 
-    def test_up_to_date_old_file(self):
+    def test_up_to_date_old_file(self) -> None:
+        """Test that up_to_date returns False if the file is older than the timestamp."""
         file = PavedPath("Temp Test Files/test_up_to_date_old_file.ext")
         file.write("Text")
-        timestamp = datetime.now()
+        timestamp = datetime.now().astimezone()
         assert not file.up_to_date(timestamp)
         file.parent.delete()
 
 
 class TestWrite:
-    def test_write_binary(self):
+    """Test the write method."""
+
+    def test_write_binary(self) -> None:
+        """Test that write_binary writes bytes to a file."""
         file = PavedPath("Temp Test Files/test_write_binary.ext")
         file.write(b"Text")
         assert file.read_bytes() == b"Text"
         file.parent.delete()
 
-    def test_write_text(self):
+    def test_write_text(self) -> None:
+        """Test that write_text writes a string to a file."""
         file = PavedPath("Temp Test Files/test_write_text.ext")
         file.write("Text")
         assert file.read_text(encoding="utf-8") == "Text"
@@ -71,19 +84,24 @@ class TestWrite:
 
 
 class TestDelete:
-    def delete_file(self):
+    """Test the delete method."""
+
+    def delete_file(self) -> None:
+        """Test that delete deletes a file."""
         file = PavedPath("Temp Test Files/test_delete_file.ext")
         file.write("Text")
         file.delete()
         assert file.exists()
 
-    def delete_empty_folder(self):
+    def delete_empty_folder(self) -> None:
+        """Test that delete deletes an empty folder."""
         folder = PavedPath("Temp Test Files/test_delete_empty_folder")
         folder.mkdir()
         folder.delete()
         assert folder.exists()
 
-    def delete_non_empty_folder(self):
+    def delete_non_empty_folder(self) -> None:
+        """Test that delete deletes a non-empty folder."""
         folder = PavedPath("Temp Test Files/test_delete_non_empty_folder")
         folder.mkdir()
         file = folder / "test_delete_non_empty_folder.ext"
@@ -93,45 +111,54 @@ class TestDelete:
 
 
 class TestCachedRead:
-    def make_initial_file(self):
+    """Test the cached read methods."""
+
+    def make_initial_file(self) -> PavedPath:
+        """Create a file with initial content."""
         file = PavedPath("tests/test_cached_read.txt")
         file.write("123")
         return file
 
-    def test_read_text(self):
+    def test_read_text(self) -> None:
+        """Test that read_text reads a file."""
         file = self.make_initial_file()
         assert file.read_text_cached() == "123"
         file.delete()
 
-    def test_read_text_changed_file(self):
+    def test_read_text_changed_file(self) -> None:
+        """Test that read_text caches the initial content and does not update it when the file changes."""
         file = self.make_initial_file()
         file.read_text_cached()
         file.write("456")
         assert file.read_text_cached() == "123"
         file.delete()
 
-    def test_read_text_updating_cache(self):
+    def test_read_text_updating_cache(self) -> None:
+        """Test that force_read_text_cached updates the cache."""
         file = self.make_initial_file()
         file.read_text_cached()
         file.write("456")
-        assert file.read_text_cached(True) == "456"
+        assert file.force_read_text_cached() == "456"
         file.delete()
 
-    def test_read_bytes(self):
+    def test_read_bytes(self) -> None:
+        """Test that read_bytes reads a file."""
         file = self.make_initial_file()
         assert file.read_bytes_cached() == b"123"
         file.delete()
 
-    def test_read_bytes_changed_file(self):
+    def test_read_bytes_changed_file(self) -> None:
+        """Test that read_bytes caches the initial content and does not update it when the file changes."""
         file = self.make_initial_file()
         file.read_bytes_cached()
         file.write("456")
         assert file.read_bytes_cached() == b"123"
         file.delete()
 
-    def test_read_bytes_updating_cache(self):
+    def test_read_bytes_updating_cache(self) -> None:
+        """Test that force_read_bytes_cached updates the cache."""
         file = self.make_initial_file()
         file.read_bytes_cached()
         file.write("456")
-        assert file.read_bytes_cached(True) == b"456"
+        assert file.force_read_bytes_cached() == b"456"
         file.delete()
